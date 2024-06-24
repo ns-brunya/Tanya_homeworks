05da506036e3177c4aec5539603bd1ba56885784
ns-brunya <wowarzamasova@yandex.ru>
2024-06-24 20:45:32 +0300

    homework4

---
+++ main.py
@@ -0,0 +1,8 @@
+my_string=input('Задайте строковую переменную: ')
+print('длина введенной строки: ',len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string[0])
+print(my_string[len(my_string)-1])
+
+
